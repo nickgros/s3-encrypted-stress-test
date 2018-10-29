20e468b0f1e43d5d4701446e38368986706a4291
Nick Grosenbacher <nickgrosenbacher@gmail.com>
2018-10-29 16:45:18 -0700

    fix startup script

--- ec2-startup.sh
+++ ec2-startup.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
 sudo yum upgrade -y
-sudo yum install git -y
+sudo yum install bzip2-devel gcc git libssl-dev readline-devel zlib-devel -y
+
+sudo yum install gcc git zlib-devel bzip2 bzip2-devel readline-devel sqlite sqlite-devel openssl-devel xz xz-devel libffi-devel -y
 
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
 echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
 echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.bashrc
 exec "$SHELL"
 
-pyenv install 3.6.6
+curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
 
-pip install -u pip
-pip install -u numpy pandas requests synapseclient
+pyenv install 3.6.7
+pyenv global 3.6.7
+
+pip install --upgrade pip
+pip install --upgrade numpy pandas requests synapseclient

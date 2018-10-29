c4ed308133308e55235ea0371da8e74a01fb58b6
Nick Grosenbacher <nickgrosenbacher@gmail.com>
2018-10-29 15:23:15 -0700

    initial commit

---
+++ stressTest.py
@@ -0,0 +1,45 @@
+import os, sys
+import stressTestFunctions as util
+import pandas as pd
+import synapseclient
+from synapseclient import File
+
+syn = synapseclient.login()
+
+encryptedProjectId = 'syn17016492'
+unencryptedProjectId = 'syn17016502'
+encryptedBucketName = 'plfm-5212-encrypted-bucket'
+unencryptedBucketName = 'plfm-5212-unencrypted-bucket'
+
+# setProjectBucket(encryptedProjectId, encryptedBucketName)
+# setProjectBucket(unencryptedProjectId, unencryptedBucketName)
+
+def upload_download_test(num_files, file_size_kb):
+    # Empty the project space beforehand
+    util.deleteAllFilesFromProject(unencryptedProjectId)
+    util.deleteAllFilesFromProject(encryptedProjectId)
+
+    # Test the unencrypted bucket and empty it
+    unencrypted_up_times = util.uploadStressTest(num_files, file_size_kb, unencryptedProjectId)
+    unencrypted_down_times = util.downloadStressTest(unencryptedProjectId)
+    util.deleteAllFilesFromProject(unencryptedProjectId)
+
+    # Test the encrypted bucket and empty it
+    encrypted_up_times = util.uploadStressTest(num_files, file_size_kb, encryptedProjectId)
+    encrypted_down_times = util.downloadStressTest(encryptedProjectId)
+    util.deleteAllFilesFromProject(encryptedProjectId)
+
+    # Assemble the dataframe and return it
+    df = pd.DataFrame({"time": unencrypted_up_times, "bucket": "unencrypted", "type": "upload", "file_size": file_size_kb})
+    df = df.append(pd.DataFrame({"time": encrypted_up_times, "bucket": "encrypted", "type": "upload", "file_size": file_size_kb}))
+    df = df.append(pd.DataFrame({"time": unencrypted_down_times, "bucket": "unencrypted", "type": "download", "file_size": file_size_kb}))
+    df = df.append(pd.DataFrame({"time": encrypted_down_times, "bucket": "encrypted", "type": "download", "file_size": file_size_kb}))
+    return df
+
+df = pd.DataFrame()
+
+# Test 1KB, 1MB, 5MB, 50MB, 100MB, 1GB
+for file_size_kb in [1, 1024, 1024 * 5, 1024 * 50, 1024 * 100, 1024 * 1024]:
+    df = df.append(upload_download_test(num_files=10, file_size_kb=file_size_kb))
+
+df.to_csv("output_data.csv", index = False)
---
+++ stressTestFunctions.py
@@ -0,0 +1,79 @@
+import os, sys
+import json
+import time
+import numpy as np
+import synapseclient
+from synapseclient import File
+from subprocess import call
+
+# If being throttled by Synapse:
+# This will make multipart file uploads slower as we will only get 1 presigned URL at a time
+# synapseclient.config.single_threaded = True
+
+
+syn = synapseclient.login()
+
+def setProjectBucket(project_id, bucket_name):
+    destination = {
+                   'uploadType':'S3', 
+                   'concreteType':'org.sagebionetworks.repo.model.project.ExternalS3StorageLocationSetting',
+                   'bucket': bucket_name
+                  }
+    destination = syn.restPOST('/storageLocation', body=json.dumps(destination))
+    project_destination = {
+                           'concreteType': 'org.sagebionetworks.repo.model.project.UploadDestinationListSetting', 
+                           'settingsType': 'upload'
+                          }
+    project_destination['locations'] = [destination['storageLocationId']]
+    project_destination['projectId'] = project_id
+    project_destination = syn.restPOST('/projectSettings', body = json.dumps(project_destination))
+
+def createDummyFile(size_in_kb, name):
+    if not os.path.exists("./dummy"):
+        os.makedirs("./dummy")
+    # We make the data pseudorandom to avoid unknown optimizations when uploading/downloading something like /dev/zero
+    # This is a relatively speedy way to do it (though not very portable).
+    # See: https://superuser.com/questions/792427/creating-a-large-file-of-random-bytes-quickly/792505
+    call("dd if=<(openssl enc -aes-256-ctr -pass pass:\"$(dd if=/dev/urandom bs=128 count=1 2>/dev/null | base64)\" -nosalt < /dev/zero) of=./dummy/" + name + " bs=1024 count=" + str(size_in_kb), shell=True, executable="/bin/bash")
+
+# Returns time it took to store the file
+def timedFileUpload(filepath, project_id):
+    file = File(path=filepath, parent=project_id)
+    start = time.time()
+    file = syn.store(file)
+    end = time.time()
+    return(end - start)
+
+
+def timedFileDownload(entity):
+    start = time.time()
+    syn.get(entity)
+    end = time.time()
+    return(end - start)
+
+# Returns list of times it took to upload files
+def uploadStressTest(num_files, file_size_kb, project_id):
+    # create n files
+    for i in range(num_files):
+        createDummyFile(file_size_kb, "output_" + str(i) + ".dat")
+    
+    times = []
+    for i in range(num_files):
+        times.append(timedFileUpload("./dummy/output_" + str(i) + ".dat", project_id))
+        
+    return times
+
+def downloadStressTest(project_id):
+    times = []
+    for entity in syn.getChildren(project_id):
+        times.append(timedFileDownload(entity["id"]))
+    
+    return times
+
+def deleteAllFilesFromProject(project_id):
+    for entity in syn.getChildren(project_id):
+        syn.delete(entity["id"])
+    
+def saveToCsv(array, outfile):
+    np.savetxt(outfile, np.array(array), delimiter=",")
+
